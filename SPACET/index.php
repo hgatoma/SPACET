6bc05a691ab46425950d20039246ea6100b963f7
hgatoma <55154665+hgatoma@users.noreply.github.com>
2019-09-10 19:18:01 +0200

    Add files via upload

---
+++ SPACET/index.php
@@ -0,0 +1,16 @@
+<?php
+
+// ##############################################
+//index
+//###############################################
+
+include_once('_cabecalho.php');
+
+echo '<h2 class ="text-center"> OLA SPACET</h2>';
+
+include_once('_rodape.php');
+
+
+
+?>
+
